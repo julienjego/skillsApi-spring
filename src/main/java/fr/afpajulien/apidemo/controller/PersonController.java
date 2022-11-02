30c585365835238093c859254b7b03003ec438e5
julienjego <jego.jj@gmail.com>
2022-11-02 10:07:54 +0100

    Fix imports

--- src/main/java/fr/afpajulien/apidemo/controller/PersonController.java
+++ src/main/java/fr/afpajulien/apidemo/controller/PersonController.java
@@ -3,7 +3,6 @@ package fr.afpajulien.apidemo.controller;
 import java.util.Iterator;
 import java.util.List;
 
-import org.hibernate.annotations.SourceType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
